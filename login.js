174af4aecfa0ead043de649654b8ad24a5170d0b
Malik Samansya <maliksamansya206@gmail.com>
2023-04-06 21:55:23 +0700

    login page

---
+++ login.js
@@ -0,0 +1,9 @@
+const usernameField = document.querySelector(".login__input_user");
+// console.log(usernameField);
+const passwordField = document.querySelector(".login__input_pass");
+// console.log(passwordField);
+function validator() {
+  if (usernameField.value === "peter" && passwordField.value === "1111") {
+    window.open("index.html", "_blank");
+  }
+}
